83bf61f15de57acd56c9048fdca7d5ad545f3151
Samarkin Danil <samarkin.danil@yandex.ru>
2024-08-14 18:40:13 +0300

    for deploy

--- src/js/modules/parallax.js
+++
@@ -1,9 +0,0 @@
-const parallax = (sceneSelector) => {
-    const scene = document.querySelectorAll(sceneSelector);
-
-    scene.forEach((item) => {
-        new Parallax(item);
-    });
-};
-
-export default parallax;
